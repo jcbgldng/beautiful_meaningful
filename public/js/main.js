d5ff4564aaeb5893292dc53646c307d180f69635
Jacob Golding <jcbgldng@gmail.com>
2016-08-18 15:56:46 -0500

    created first platform"

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,25 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
-	game.load.image('background', 'assets/sky1.png')	
+	game.load.image('sky', 'assets/bg_layer1.png')
+	game.load.image('land', 'assets/ground_grass_small .png')
 }
+
+var land;
 
 function create() {
 		
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 
-	game.add.sprite(0, 0, 'background');
+	game.add.sprite(0, 0, 'sky');
+
+	land = game.add.group();
+
+	land.enableBody = true;
+
+	var platform = land.create(0, game.world.height - 64, 'land')
+	platform.scale.setTo(.75, .75);
+
 }
 
 function update() {
