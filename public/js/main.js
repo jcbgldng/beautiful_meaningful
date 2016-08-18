9d96a8a3dd3f9d393a407ef1e84d49ccffd8130a
Jacob Golding <jcbgldng@gmail.com>
2016-08-18 16:12:21 -0500

    added the player sprite to the game

--- public/js/main.js
+++ public/js/main.js
@@ -3,9 +3,11 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 function preload() {
 	game.load.image('sky', 'assets/bg_layer1.png')
 	game.load.image('land', 'assets/ground_grass_small .png')
+	game.load.image('player', 'assets/flyMan_stand.png')
 }
 
 var land;
+var player;
 
 function create() {
 		
@@ -28,6 +30,15 @@ function create() {
 	platform = land.create(600, 250, 'land')
 	platform.scale.setTo(.75, .75);
 	platform.body.immovable = true;
+
+	player = game.add.sprite(0, game.world.height -150, 'player')
+	player.scale.setTo(.5, .5);
+
+	game.physics.arcade.enable(player);
+	player.body.gravity.y = 300;
+	player.body.collideWorldBounds = true;
+
+
 }
 
 function update() {
